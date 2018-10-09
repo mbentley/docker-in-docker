09dc05b9440f9e26a0a7ea5f5e6cb247282411ce
Matt Bentley <mbentley@mbentley.net>
2018-10-09 15:03:45 -0400

    Fixes for beta3
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- install_containerd_binaries.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-containerd &
-/usr/libexec/containerd-offline-installer /var/lib/containerd-offline-installer/runc.tar docker.io/docker/runc
-/usr/libexec/containerd-offline-installer /var/lib/containerd-offline-installer/containerd-shim-process.tar docker.io/docker/containerd-shim-process
-kill "$(pidof containerd)"
