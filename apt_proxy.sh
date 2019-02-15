4c8e82b25a23661007f2e95f414c6250cffc1005
Matt Bentley <mbentley@mbentley.net>
2019-02-15 11:26:51 -0500

    Added apt-cache support
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

---
+++ apt_proxy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+if [ -n "${APT_PROXY}" ]
+then
+  echo -n "Setting apt proxy..."
+  echo 'Acquire::http::Proxy "http://'"${APT_PROXY}"'";' > /etc/apt/apt.conf.d/00proxy
+  echo "done"
+fi
