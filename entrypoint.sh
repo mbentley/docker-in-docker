01cf7c22532ed90b40d65474aa26283732af0298
Matt Bentley <mbentley@mbentley.net>
2020-02-05 09:46:27 -0500

    Manually start containerd
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,8 @@ then
   rm  /var/run/docker.pid
 fi
 
-CONTAINERD_RUNNING="$(pgrep docker-containerd > /dev/null 2>&1; echo $?)"
-if [ -f "/var/run/docker/libcontainerd/docker-containerd.pid" ] && [ "${CONTAINERD_RUNNING}" -eq "1" ]
-then
-  echo "Removing stale pid file (/var/run/docker/libcontainerd/docker-containerd.pid)..."
-  rm /var/run/docker/libcontainerd/docker-containerd.pid
-fi
-
 # set mount propagation
-if [ ! -z "${MOUNT_PROPAGATION}" ]
+if [ -n "${MOUNT_PROPAGATION}" ]
 then
   for MOUNT in ${MOUNT_PROPAGATION}
   do
@@ -23,5 +16,14 @@ then
     mount --make-rshared "${MOUNT}"
   done
 fi
+
+# start containerd because docker will start it differently than systemd would have
+/usr/bin/containerd &
+
+# wait to make sure containerd starts
+while [ ! -S "/run/containerd/containerd.sock" ]
+do
+  sleep .25
+done
 
 exec "${@}"
