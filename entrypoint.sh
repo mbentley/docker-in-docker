5a18c4ae25afb06b60d5a9f1717f442444d52272
Matt Bentley <mbentley@mbentley.net>
2020-02-25 10:04:09 -0500

    Add in fix for multiple versions
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,13 +17,25 @@ then
   done
 fi
 
-# start containerd because docker will start it differently than systemd would have
-/usr/bin/containerd &
+# check to see if /usr/bin/containerd exists; if not; we are probably running an older version
+if [ ! -f /usr/bin/containerd ]
+then
+  # /usr/bin/containerd doesn't exist; expect that it is packaged with the engine
+  CONTAINERD_RUNNING="$(pgrep docker-containerd > /dev/null 2>&1; echo $?)"
+  if [ -f "/var/run/docker/libcontainerd/docker-containerd.pid" ] && [ "${CONTAINERD_RUNNING}" -eq "1" ]
+  then
+      echo "Removing stale pid file (/var/run/docker/libcontainerd/docker-containerd.pid)..."
+        rm /var/run/docker/libcontainerd/docker-containerd.pid
+  fi
+else
+  # /usr/bin/containerd exists; we should start containerd because docker will start it differently than systemd would have
+  /usr/bin/containerd &
 
-# wait to make sure containerd starts
-while [ ! -S "/run/containerd/containerd.sock" ]
-do
-  sleep .25
-done
+  # wait to make sure containerd starts
+  while [ ! -S "/run/containerd/containerd.sock" ]
+  do
+    sleep .25
+  done
+fi
 
 exec "${@}"
