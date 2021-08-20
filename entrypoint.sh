2ca32cc8da71c76f3d4bb4a726bf2ed3d0fe5b5e
Matt Bentley <mbentley@mbentley.net>
2021-08-20 07:58:22 -0400

    Handle cgroups v2
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,4 +38,18 @@ else
   done
 fi
 
+# cgroup v2: enable nesting (from https://github.com/moby/moby/blob/v20.10.8/hack/dind#L28-L38)
+if [ -f /sys/fs/cgroup/cgroup.controllers ]
+then
+  echo "cgroups v2 detected; enabling nesting"
+  # move the processes from the root group to the /init group,
+  # otherwise writing subtree_control fails with EBUSY.
+  # An error during moving non-existent process (i.e., "cat") is ignored.
+  mkdir -p /sys/fs/cgroup/init
+  xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
+  # enable controllers
+  sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers \
+    > /sys/fs/cgroup/cgroup.subtree_control
+fi
+
 exec "${@}"
